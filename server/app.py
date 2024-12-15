762ab6fc3b4aca5db235f308ba283876b7b8711a
MatthewLeaman <leaman.30@osu.edu>
2024-12-15 03:49:48 -0500

    complete

--- server/app.py
+++ server/app.py
@@ -6,6 +6,7 @@ from flask_restful import Resource
 from config import app, db, api
 from models import User
 
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -28,16 +29,45 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict(), 200
+        return {}, 204
+
 
 class Login(Resource):
-    pass
+
+    def post(self):
+
+        json = request.get_json()
+
+        username = json['username']
+        user = User.query.filter(User.username == username).first()
+
+        password = json['password']
+
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {'error': 'Invalid username or password'}, 401
+
 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login')
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Logout, '/logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
